12090a3bc5665955f0e9e8d25acd93b78be5dd30
Yahya <yahyamohmuedpro99@gmail.com>
2025-03-27 00:47:49 +0200

    Remove file logging and associated log files to streamline logging configuration

--- ai_csv_transformer.py
+++ ai_csv_transformer.py
@@ -17,8 +17,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler('transformer.log')
+        logging.StreamHandler()
     ]
 )
 
--- web_interface.py
+++ web_interface.py
@@ -14,8 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler('app.log')
+        logging.StreamHandler()
     ]
 )
 
